3e426abf5f863820a20a9308e71dec4dc6081e54
ISDementyev <ilyasd@protonmail.com>
2025-03-05 12:27:23 -0500

    Added func for reading and tokenizing corpus

--- utils.py
+++ utils.py
@@ -1,4 +1,20 @@
 import nltk
+from nltk.tokenize import word_tokenize
+
+
+def read_corpus(fname):
+    """
+    Reads corpus from a file containing it.
+    :param fname: Name of the file (str)
+    :return: File contents (corpus) (str)
+    """
+
+    with open(fname) as f:
+        return f.read()
 
 if __name__ == "__main__":
-    pass
+    nltk.download("punkt_tab")
+    akjv_file = "/Users/ilya/Desktop/DL_AI/shsp_or_kjb/kjb_corpus/AKJV.txt"
+    akjv_corpus = read_corpus(akjv_file)
+    tokenized_akjv = word_tokenize(akjv_corpus)
+    print(type(tokenized_akjv))
